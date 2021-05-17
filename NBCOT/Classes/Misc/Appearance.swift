0763b4934b2c4e271935f3046d5700c3bbd91b82
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-17 13:32:46 +0300

    copy

---
+++ NBCOT/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 255, green: 250, blue: 253)
+    
+    static let mainColor = UIColor(integralRed: 132, green: 41, blue: 98)
+    static let mainColorAlpha = UIColor(integralRed: 132, green: 41, blue: 98, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 254, green: 50, blue: 58)
+    
+    static let progress1Color = UIColor(integralRed: 132, green: 41, blue: 98)
+    static let progress2Color = UIColor(integralRed: 254, green: 50, blue: 58)
+    static let progress3Color = UIColor(integralRed: 254, green: 215, blue: 49)
+}
