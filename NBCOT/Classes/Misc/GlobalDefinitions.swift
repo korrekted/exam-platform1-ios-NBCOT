237aac6242be8ad46af28db317b9f50643203301
akonst17 <akonst17@gmail.com>
2022-09-20 11:25:16 +0300

    интеграция sdk 2.0; новые урлы; build 15

--- NBCOT/Classes/Misc/GlobalDefinitions.swift
+++ NBCOT/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams.mastrapi.com"
     static let apiKey = "VjsptHCcIl5N1jKB"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "c7E6CNum7zsqZKFs"
-
     static let applicationTag = "EXAM_NBCOT"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "c7E6CNum7zsqZKFs"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "PvvF6TaCIn0X8mAm"
 
     static let appStoreUrl = "https://apps.apple.com/app/nbcot-occupational-therapy/id1567717846"
     static let appleAppID = "1567717846"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
